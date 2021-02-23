329eedd5b99f65f168aa7d045afceec700373cce
joao vitor resende <54758120+JoaoVitorResende@users.noreply.github.com>
2021-02-22 23:20:00 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { ExperienceBar } from './Componentes/ExperienceBar';
+import'./Styles/global.css';
+
+function App() {
+  return(
+    <div className = "container">
+
+    <ExperienceBar/>
+
+    </div>
+  )
+}
+
+export default App;
